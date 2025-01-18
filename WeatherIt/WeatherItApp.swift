538f922f5d3dc3fa72829fbccdaa3b7ceebf16bf
Khondwani Sikasote <Khondwani66@gmail.com>
2025-01-18 23:03:39 +0200

    Initial Commit

---
+++ WeatherIt/WeatherItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherItApp.swift
+//  WeatherIt
+//
+//  Created by Khondwani Sikasote on 2025/01/18.
+//
+
+import SwiftUI
+
+@main
+struct WeatherItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
